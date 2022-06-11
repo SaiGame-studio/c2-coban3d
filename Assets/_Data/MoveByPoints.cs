65992149d1508c511de81994d3c77ad9ba0034f0
SimonHome <tranminhduc18116@gmail.com>
2022-06-11 23:34:36 +0700

    Day 4

--- Assets/_Data/MoveByPoints.cs
+++ Assets/_Data/MoveByPoints.cs
@@ -1,21 +1,28 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveByPoints : MonoBehaviour
 {
+    public float speed = 1f;
+    public int pointIndex = 0;
+    public float pointDistance = Mathf.Infinity;
     public Transform pointsHolder;
-    public int pointInput = 0;
     public List<Transform> points;
 
-    void Start()
+    private void Start()
     {
         this.LoadPoints();
     }
 
+    private void Update()
+    {
+        //this.PointLerpMoving();
+        this.PointMoving();
+    }
+
     private void FixedUpdate()
     {
-        
+        this.NextPointCalculate();
     }
 
     protected virtual void LoadPoints()
@@ -26,5 +33,31 @@ public class MoveByPoints : MonoBehaviour
         {
             this.points.Add(point);
         }
+    }
+
+    protected virtual void PointLerpMoving()
+    {
+        float lerpSpeed = 0.1f;
+        Transform currentPoint = this.points[this.pointIndex];
+        transform.position = Vector3.Lerp(transform.position, currentPoint.position, lerpSpeed);
+    }
+
+    protected virtual void PointMoving()
+    {
+        float step = this.speed * Time.deltaTime;
+        Transform currentPoint = this.CurrentPoint();
+        transform.position = Vector3.MoveTowards(transform.position, currentPoint.position, step);
+    }
+
+    protected virtual void NextPointCalculate()
+    {
+        this.pointDistance = Vector3.Distance(transform.position, this.CurrentPoint().position);
+        if (this.pointDistance == 0) this.pointIndex++;
+        if (this.pointIndex >= this.points.Count) this.pointIndex = 0;
+    }
+
+    public virtual Transform CurrentPoint()
+    {
+        return this.points[this.pointIndex];
     }
 }
